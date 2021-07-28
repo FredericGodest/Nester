ad7cd35300e6c7d5eaa0851072f549c6023954ab
Clement Avenel <contact@clement-avenel.com>
2021-07-28 13:20:50 +0200

    Changement de logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,22 @@
 import {
-    Box,
-    Grid,
     Text,
+    Stack,
+    Flex
   } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function NavBar(){
     return(
-        <Box m={5}>
-            <Grid templateColumns="repeat(2, 1fr)" gap={3}>
-                <Text fontSize="2xl" textAlign="left ">
-                    <img width='20%' height='auto' src='/logo.png' alt="logo" />
-                </Text>
-                <ColorModeSwitcher justifySelf="flex-end" />
-            </Grid>
-        </Box>
+        <Stack direction={'row'} p={'2vw'}>
+          <Flex flex={1} align={'center'} justify={'center'}>
+           <Text fontSize="2xl" textAlign="left ">
+               <img width='30%' height='auto' src='/logo-nester.png' alt="logo" />
+           </Text>
+          </Flex>
+          <Flex flex={1} align={'center'} justify={'flex-end'}>
+            <ColorModeSwitcher justifySelf="flex-end" mx={'5px'}/>
+          </Flex>
+        </Stack>
 
     )
 };
